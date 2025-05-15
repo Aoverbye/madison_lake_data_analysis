65273c4f8ad9754132d511aec326857e4e60e6a8
Aoverbye <amandaoverbye@gmail.com>
2025-05-14 19:53:11 -0500

    Added query.

--- data/wisconsin_data.sql
+++ data/wisconsin_data.sql
@@ -1,16 +1,4 @@
 
--- Table: air_temp
-CREATE TABLE air_temp (
-    year                   INTEGER,
-    sampledate              DATE PRIMARY KEY,
-    max_air_temp_adjusted   FLOAT,
-    min_air_temp_adjusted   FLOAT,
-    avg_air_temp_adjusted   FLOAT,
-    range_air_temp_adjusted FLOAT,
-    precip_raw_mm           FLOAT,
-    snow_raw_cm             FLOAT,
-    FOREIGN KEY (year) REFERENCES year(year)
-);
 
 -- Table: year
 CREATE TABLE year (
@@ -22,14 +10,36 @@ CREATE TABLE year (
     avg_ice_duration             FLOAT
 );
 
+-- Table: air_temp
+CREATE TABLE air_temp (
+    year                   INTEGER,
+    sampledate              DATE PRIMARY KEY,
+    max_air_temp_adjusted   FLOAT,
+    min_air_temp_adjusted   FLOAT,
+    range_air_temp_adjusted FLOAT,
+    precip_raw_mm           FLOAT,
+    snow_raw_cm             FLOAT,
+    FOREIGN KEY (year) REFERENCES year(year)
+);
+
 CREATE TABLE lake_ice(
-    lakeid     INTEGER,
+    lakeid     TEXT,
     year       INTEGER,
     ice_on     DATE,
     ice_off    DATE,
     duration   INTEGER
 );
 
-COPY air_temp FROM 'air_temp_clean.csv' (Header TRUE);
-COPY lake_ice FROM 'lake_ice_clean.csv' (Header TRUE);
-COPY year FROM 'year.csv' (Header TRUE);
+COPY year FROM 'year.csv' (HEADER TRUE);
+COPY air_temp FROM 'air_temp_clean.csv' (HEADER TRUE);
+COPY lake_ice FROM 'lake_ice_clean.csv' (HEADER TRUE);
+
+
+-- Question: Are minimum air temperatures related to the duration of ice cover?
+
+SELECT 
+  year, 
+  avg_min_air_temp_adjusted, 
+  avg_ice_duration
+FROM year
+ORDER BY year;
